f121888d1bf5b187306a8b84b9033dacd7a5deec
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-04-23 10:39:50 -0400

    feat: Prepare for marketplace

--- flayyer.config.js
+++ flayyer.config.js
@@ -1,6 +1,7 @@
 // Created with create-flayyer-app@1.15.2
 
 const {config} = require('@flayyer/flayyer-types');
+const dedent = require('dedent');
 require('dotenv').config();
 
 module.exports = config({
@@ -10,6 +11,14 @@ module.exports = config({
 
   // Optionals
   name: 'Default deck',
-  description:
-    'This is the default deck created with create-flayyer-app to showcase how to start your own set of templates.'
+  description: dedent`
+  This is the default deck created with create-flayyer-app to showcase how to start your own set of templates.
+  `,
+  marketplace: true,
+  homepage: 'https://flayyer.com',
+  keywords: ['flayyer', 'free', 'fade', 'contrast'],
+  license: 'MIT',
+  private: false,
+  repository: 'https://github.com/flayyer/flayyer-marketplace-default',
+  sizes: ['THUMBNAIL', 'BANNER', 'SQUARE', 'STORY', 'FREE']
 });
